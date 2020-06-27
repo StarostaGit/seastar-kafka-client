c370fedf548dabbfce2bab24905d4e57c42ac5ff
StarostaGit <piotr.m.wojtczak@gmail.com>
2020-06-27 21:50:29 +0200

    Producer properties and settings
    This change adds properties used customize and set up a producer client
    instance.
    
    	`producer_properties` provides an object grouping all possible
    	settings. Details about each field are provided in the file itself
    	(`producer_properties.hh`). This should be used by the end user
    	to customize the client.
    
    	`defaults` provides a couple of predefined values and strategies
    	that can be set in the properties.
    
    	`partitioner` provides two common strategies for partitioning -
    	a round robin strategy and a random partitioner.

---
+++ include/kafka4seastar/utils/defaults.hh
@@ -0,0 +1,44 @@
+/*
+ * This file is open source software, licensed to you under the terms
+ * of the Apache License, Version 2.0 (the "License").  See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership.  You may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/*
+ * Copyright (C) 2019 ScyllaDB Ltd.
+ */
+
+#pragma once
+
+#include <functional>
+#include <memory>
+
+#include <seastar/core/future.hh>
+#include <seastar/util/noncopyable_function.hh>
+
+#include <kafka4seastar/utils/partitioner.hh>
+
+namespace kafka4seastar {
+
+namespace defaults {
+
+seastar::noncopyable_function<seastar::future<>(uint32_t)> exp_retry_backoff(uint32_t base_ms, uint32_t max_backoff_ms);
+
+std::unique_ptr<partitioner> round_robin_partitioner();
+std::unique_ptr<partitioner> random_partitioner();
+
+}
+
+}
